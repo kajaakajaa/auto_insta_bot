2ddfb7ef72d65f826d33d3f883f4ed361af8b49c
kajaakajaa <rwqxh8703@yahoo.co.jp>
2021-06-07 15:44:05 +0900

    ログイン・ログアウト実装OK

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   root "pages#index"
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: "users/registrations",
+    sessions: "users/sessions"
+  }
   devise_scope :user do
-    get "/users/sign_out" => "devise/sessions#destroy"
+    get "/users/sign_out" => "users/sessions#destroy"
   end
 
   resources :users, only: %i[destroy]
