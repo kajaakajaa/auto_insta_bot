23e21931891c1e53622a6b14941b6a86c3a76bb7
kajaakajaa <rwqxh8703@yahoo.co.jp>
2021-07-20 11:43:05 +0900

    checkboxのon時に適当な値をベタ書きで渡す

--- app/controllers/instabots_controller.rb
+++ app/controllers/instabots_controller.rb
@@ -8,14 +8,34 @@ class InstabotsController < ApplicationController
   
   # instabots_path POST
   def create
-    # session[:instabot] = Instabot.new(instabot_params)
-    @auth = Instabot.new(instabot_params)
+    # @auth = Instabot.new(instabot_params)
+    session[:instabot] = Instabot.new(instabot_params)
     insta_sign_in
+
+    #createに置くと正常に動く
+    # key_word = "oneokrock"
+    # good_hashtag(key_word,3)
+  end
+
+  # instabot_good_path POST
+  def good
+    @instabot = Instabot.new(good_params)
+
+    key_word = "oneokrock"
+    number = 3
+
+    if @instabot.good == true
+      good_hashtag(key_word,number)
+    end
   end
 
   private
   def instabot_params
-    params.require(:instabot).permit(:user_name, :password)
+    params.require(:session).permit(:user_name, :password)
+  end
+
+  def good_params
+    params.require(:session).permit(:good)
   end
 end
 
--- db/schema.rb
+++ db/schema.rb
@@ -17,6 +17,7 @@ ActiveRecord::Schema.define(version: 2021_07_11_001854) do
     t.string "password", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "good", default: false, null: false
   end
 
   create_table "users", force: :cascade do |t|
