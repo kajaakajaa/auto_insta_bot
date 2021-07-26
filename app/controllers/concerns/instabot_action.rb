47dbcb291e723a3e4078ea696c3dad5fd3860ded
kajaakajaa <rwqxh8703@yahoo.co.jp>
2021-07-27 08:16:21 +0900

    2021/7/27_16mac

--- app/controllers/concerns/instabot_action.rb
+++ app/controllers/concerns/instabot_action.rb
@@ -61,11 +61,6 @@ module InstabotAction
         sleep 3
         @driver.navigate.to"https://www.instagram.com/explore/tags/#{encode_word}/"
         sleep 3
-        # 自動フォローの呼び出し
-        self.follow
-        sleep 3
-        # unfollow
-        # sleep 3
         @driver.execute_script("document.querySelectorAll('article img')[9].click()")
         sleep 3
         number.times{
@@ -81,15 +76,41 @@ module InstabotAction
           sleep 3
         }
       else
-        render root_path
+        puts "いいね check_box_はoffです"
       end
     end
 
-    def follow
-      begin
-        @driver.execute_script("document.querySelector(`button.y3zKF`).click()")
-      rescue
-        puts "このタグは既にフォロー済みです"
+    def follow(key_word)
+      if @instabot_rcd.follow == true
+        username = session[:instabot]["user_name"]
+        password = session[:instabot]["password"]
+        
+        Selenium::WebDriver::Chrome::Service
+        # 送信側のos状況をまとめたもの
+        ua = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36'
+        # シークレットモード
+        caps = Selenium::WebDriver::Remote::Capabilities.chrome('goog:chromeOptions' => { args: ["--user-agent=#{ua}", 'window-size=1280x800', '--incognito'] })
+        client = Selenium::WebDriver::Remote::Http::Default.new
+        client.read_timeout = 300
+        @driver = Selenium::WebDriver.for :chrome, http_client: client, desired_capabilities: caps
+        @driver.manage.timeouts.implicit_wait = 30
+        @driver.navigate.to'https://www.instagram.com/accounts/login/?source=auth_switcher'
+        sleep 5
+        @driver.find_element(:name, 'username').send_keys(username)
+        @driver.find_element(:name, 'password').send_keys(password)
+        @driver.find_element(:name, 'password').send_keys(:return)
+        sleep 5
+        encode_word = URI.encode_www_form_component(key_word)
+        sleep 3
+        @driver.navigate.to"https://www.instagram.com/explore/tags/#{encode_word}/"
+        sleep 3
+        begin
+          @driver.execute_script("document.querySelector(`button.y3zKF`).click()")
+        rescue
+          puts "このタグは既にフォロー済みです"
+        end
+      else
+        puts "フォロー check_box_はoffです"
       end
     end
 
