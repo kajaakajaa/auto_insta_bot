acfd8b00217daf956e3cf50351993e7018e64095
kajaakajaa <rwqxh8703@yahoo.co.jp>
2021-08-10 18:10:55 +0900

    一回限り自動（修正後）

--- app/controllers/instabots_controller.rb
+++ app/controllers/instabots_controller.rb
@@ -14,7 +14,7 @@ class InstabotsController < ApplicationController
     random = Random.new
     hash_rcds = Hashtag.where(user_id: current_user.id)
     if hash_rcds.exists?
-      number = params[:instabot][:number].to_i
+      number = 2 #params[:instabot][:number].to_i
       hash_rcds.each do |hash_rcd|
         key_word = hash_rcd.hashtag
       
@@ -24,26 +24,19 @@ class InstabotsController < ApplicationController
           @rcd = Instabot.new
           # いいね
           if @rcd.good.to_s != params[:instabot][:good]
-            @rcd.good = params[:instabot][:good]
             @rcd.number = params[:instabot][:number]
             good_hashtag(key_word, number)
           # フォロー
           elsif @rcd.follow.to_s != params[:instabot][:follow]
-            @rcd.follow = params[:instabot][:follow]
             auto_follow(key_word)
           # アンフォロー
           elsif @rcd.unfollow.to_s != params[:instabot][:unfollow]
-            @rcd.unfollow = params[:instabot][:unfollow]
             auto_unfollow(key_word)
           end
           if hash_rcd == hash_rcds.last
             @rcd.user_id = current_user.id
             @rcd.save
-            puts "自動化スイッチを新規登録しました。"
-            puts "いいねは'#{@rcd.good}'です。"
-            puts "いいね'上限数'は'#{@rcd.number}'です。"
-            puts "フォローは'#{@rcd.follow}'です。"
-            puts "アンフォローは'#{@rcd.unfollow}'です。"
+            redirect_to root_path
           end
 
 
@@ -57,51 +50,22 @@ class InstabotsController < ApplicationController
 
           # いいね
           if @rcd.good != good_val && good_val == "true"
-            now = Time.now
-            end_time = now + 86400
-            # while Time.now < end_time do
-              # hash_rcds.each do |hash_rcd|
-                key_word = hash_rcd.hashtag
-                good_hashtag(key_word, number)
-                if hash_rcd == hash_rcds.last
-                  @rcd.update(good: good_val)
-                end
-              # end
-            # end
+            key_word = hash_rcd.hashtag
+            good_hashtag(key_word, number)
 
           # フォロー
           elsif @rcd.follow.to_s != follow_val
-            # フォロー・アンフォローの被り阻止
-            if follow_val == "true" && @rcd.unfollow == true
-              flash[:error] = "'登録済みの自動フォロー解除'をoffにしてから再度操作して下さい。"
-              respond_to do |format|
-                format.js { render ajax_redirect_to(root_path) }
-              end
-              break
-            else
-              auto_follow(key_word)
-            end
+            auto_follow(key_word)
 
           # アンフォロー
           elsif @rcd.unfollow.to_s != unfollow_val
-            # フォロー・アンフォローの被り阻止
-            if unfollow_val == "true" && @rcd.follow == true
-              flash[:error] = "'自動フォロー'をoffにしてから再度操作して下さい。"
-              respond_to do |format|
-                format.js { render ajax_redirect_to(root_path) }
-              end
-              break
-            else
-              auto_unfollow(key_word)
-            end
+            auto_unfollow(key_word)
+            
           end
           if hash_rcd == hash_rcds.last
-            @rcd.update(good: good_val, number: number_val, follow: follow_val, unfollow: unfollow_val)
-            puts "自動化スイッチを更新しました。"
-            puts "自動いいねは'#{@rcd.good}'です。"
-            puts "いいね'上限数'は'#{@rcd.number}'です。"
-            puts "フォローは'#{@rcd.follow}'です。"
-            puts "アンフォロー'#{@rcd.unfollow}'です。"
+            respond_to do |format|
+              format.js { render ajax_redirect_to(root_path) }
+            end
           end
         end
       end
